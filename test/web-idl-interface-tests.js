53582b7364b19886da7097e312823edbdbca645e
Marcos Caceres <marcos.caceres@wacapps.net>
2013-01-10 16:30:44 +0000

    The types are not exposed on the Interface Object (WebIDL) and not the instance.

--- test/web-idl-interface-tests.js
+++ test/web-idl-interface-tests.js
@@ -5,9 +5,9 @@ asyncTest("should exist in global", function(){
   setTimeout(function(){
     QUnit.start();
     ok(window.webIDL, "WebIDL is global.");
-    ok(window.webIDL.Boolean, "WebIDL Boolean type is global.");
-    ok(window.webIDL.DOMString, "WebIDL DOMString type is global.");
-    ok(window.webIDL.Double, "WebIDL Double type is global.");
+    ok(window.WebIDL.Boolean, "WebIDL Boolean type is global.");
+    ok(window.WebIDL.DOMString, "WebIDL DOMString type is global.");
+    ok(window.WebIDL.Double, "WebIDL Double type is global.");
   },200);
 
 });
