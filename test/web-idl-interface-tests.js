96fd98554e22e071afa50ce7ea39d9948a0f4032
Marcos Caceres <marcos.caceres@wacapps.net>
2013-01-11 01:29:57 +0000

    Added long and unrestricted double.

--- test/web-idl-interface-tests.js
+++ test/web-idl-interface-tests.js
@@ -1,13 +1,31 @@
+/*
+To add:
+  RegExp
+  unsigned long
+  long long
+  unsigned long long
+  float
+  unrestricted float
+  ByteString
+*/
+
 module("WebIDL Interface");
 
-asyncTest("should exist in global", function(){
-  
-  setTimeout(function(){
+asyncTest("should exist in global", function() {
+
+  setTimeout(function() {
     QUnit.start();
-    ok(window.webIDL, "WebIDL is global.");
+    ok(window.WebIDL, "WebIDL is global.");
     ok(window.WebIDL.Boolean, "WebIDL Boolean type is global.");
     ok(window.WebIDL.DOMString, "WebIDL DOMString type is global.");
     ok(window.WebIDL.Double, "WebIDL Double type is global.");
-  },200);
-
+    ok(window.WebIDL.Byte, "WebIDL Byte type is global.");
+    ok(window.WebIDL.Octet, "WebIDL Octet type is global.");
+    ok(window.WebIDL.Short, "WebIDL Short type is global.");
+    ok(window.WebIDL.UnsignedShort, "WebIDL UnsignedShort type is global.");
+    ok(window.WebIDL.Object, "WebIDL Object type is global.");
+    ok(window.WebIDL.Date, "WebIDL Date type is global.");
+    ok(window.WebIDL.Long, "WebIDL Long type is global.");
+    ok(window.WebIDL.UnrestrictedDouble, "WebIDL Unrestricted Double is global");
+  }, 200);
 });
