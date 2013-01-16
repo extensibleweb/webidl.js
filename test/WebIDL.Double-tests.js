7916919b4212e7565b5700c17a43f6265591d17d
Marcos Caceres <marcosscaceres@gmail.com>
2013-01-16 20:52:49 +0000

    Fixed a bunch of tests

--- test/WebIDL.Double-tests.js
+++ test/WebIDL.Double-tests.js
@@ -25,9 +25,7 @@ require(["types/Double"], function() {
 
 	assertion = "Let x be ToNumber(V).";
 	QUnit.test(assertion, function() {
-		QUnit.strictEqual(window.WebIDL.Double(), 0, 'ToNumber of no value is 0');
 		QUnit.strictEqual(window.WebIDL.Double(null), 0, 'null is 0');
-		QUnit.strictEqual(window.WebIDL.Double(undefined), 0, 'undefined is 0');
 		QUnit.strictEqual(window.WebIDL.Double(''), 0, 'empty string  is 0');
 		QUnit.strictEqual(window.WebIDL.Double(0), 0, 'number 0 is 0');
 		QUnit.strictEqual(window.WebIDL.Double(false), 0, 'false is 0');
@@ -35,12 +33,17 @@ require(["types/Double"], function() {
 		QUnit.strictEqual(window.WebIDL.Double(''), 0, 'empty string  is 0');
 		QUnit.strictEqual(window.WebIDL.Double(' \t\n\t '), 0, 'random whitespace is 0');
 		QUnit.strictEqual(window.WebIDL.Double('	123  '), 123, 'Whitespace removed is 123');
-		QUnit.strictEqual(window.WebIDL.Double('	123,123  '), 0, 'comma cuases NaN, which is 0');
-		QUnit.strictEqual(window.WebIDL.Double('	-123.123  '), -123, 'everything after . gets dropped');
+		QUnit.strictEqual(window.WebIDL.Double('	-123.123  '), -123.123, 'white space stripped');
 	});
 
 	assertion = "If x is NaN, +Infinity or −Infinity, then throw a TypeError.";
 	QUnit.test(assertion, function() {
+		QUnit.throws(function(){
+			WebIDL.Double();
+		}, TypeError, "undefined is NaN");
+		QUnit.throws(function(){
+			WebIDL.Double(undefined);
+		}, TypeError, "undefined is NaN");
 		QUnit.throws(function() {
 			window.WebIDL.Double(NaN);
 		}, TypeError, 'NaN throws');
@@ -49,9 +52,11 @@ require(["types/Double"], function() {
 		}, TypeError, '+Infinity throws');
 		QUnit.throws(function() {
 			window.WebIDL.Double(-Infinity);
-		}, TypeError, '-Infinity throws');
+		}, TypeError, '-Infinity throws'); 
+		QUnit.throws(function() {
+			window.WebIDL.Double('	123,123  ');
+		}, TypeError, 'comma cuases NaN, so throws');
 	});
-	
 	assertion = "Return the IDL double value that has the same numeric value as x.";
 	QUnit.test(assertion, function() {
 		QUnit.strictEqual(window.WebIDL.Double(42), 42, 'valid input does not throw');
