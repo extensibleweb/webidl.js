29a89a6d32f47dabd02ccb439d6f97924d6626c8
Marcos Caceres <marcos.caceres@wacapps.net>
2013-01-17 18:29:51 +0000

    Fixed bad tests.

--- test/WebIDL.Any-tests.js
+++ test/WebIDL.Any-tests.js
@@ -23,7 +23,6 @@ require(['types/Any'], function() {
 		teardown: function() {}
 	});
 
-
 	assertion = "The undefined value. The IDL value is an object reference to a special object that represents the ECMAScript undefined value.";
 	QUnit.test(assertion, function() {
 		QUnit.strictEqual(window.WebIDL.Any(), undefined, "undefined was returned.");
@@ -31,23 +30,21 @@ require(['types/Any'], function() {
 	});
 	assertion = "The null value. The IDL value is the null object? reference.";
 	QUnit.test(assertion, function() {
-		QUnit.strictEqual(window.WebIDL.Any(null), null);
+		QUnit.strictEqual(window.WebIDL.Any(null), null, "returns null");
 	});
 	assertion = "A Boolean value. The IDL value is the boolean value that represents the same truth value.";
 	QUnit.test(assertion, function() {
-		QUnit.strictEqual(window.WebIDL.Any(true), true);
-		QUnit.strictEqual(window.WebIDL.Any(false), false);
+		QUnit.strictEqual(window.WebIDL.Any(true), true, "needs to return true");
+		QUnit.strictEqual(window.WebIDL.Any(false), false, "needs to return false");
 	});
 	assertion = "A Number value. The IDL value is that which is obtained by following the rules for converting the Number to an IDL unrestricted double value, as described in section 4.2.15, below.";
 	QUnit.test(assertion, function() {
 		var neg0 = window.WebIDL.Any(-0);
-		QUnit.strictEqual(neg0, isNegative0(neg0), "negative 0 needs to remain negative");
-		QUnit.strictEqual(window.WebIDL.Any(1), 1);
-		QUnit.strictEqual(window.WebIDL.Any(-1), -1);
-		QUnit.strictEqual(window.WebIDL.Any(Math.pow(2,128)), +Infinity);
-		QUnit.strictEqual(window.WebIDL.Any(-Math.pow(2,128)), -Infinity);
+		QUnit.strictEqual(isNegative0(neg0), true, "negative 0 needs to remain negative");
+		QUnit.strictEqual(window.WebIDL.Any(1), 1, "needs to return 1.");
+		QUnit.strictEqual(window.WebIDL.Any(-1), -1, "needs to return -1.");
+		QUnit.strictEqual(window.WebIDL.Any(NaN), 0x7ff8000000000000, "Correct bit pattern returned");
 	});
-
 	assertion = "A String value. The IDL value is that which is obtained by following the rules for converting the String to an IDL DOMString value, as described in section 4.2.16, below.";
 	QUnit.test(assertion, function() {
 		QUnit.strictEqual(window.WebIDL.Any(""), "");
@@ -59,7 +56,6 @@ require(['types/Any'], function() {
 		QUnit.strictEqual(window.WebIDL.Any(this), this);
 		QUnit.strictEqual(window.WebIDL.Any(obj), obj);
 	});
-
 	assertion = 'The type name of the any type is “Any”.';
 	QUnit.test(assertion, function() {
 		QUnit.strictEqual(window.WebIDL.Any.prototype.type, 'Any', 'The type is “Any”.');
